f773fd5b34fd4a89c79f59acac6126551486c9d4
Jerry Hu <hh201020@gmail.com>
2016-10-03 12:47:03 -0400

    Get and update data

--- src/com/simpleprogramer/Program.java
+++ src/com/simpleprogramer/Program.java
@@ -9,13 +9,18 @@ public class Program {
 		Session session = HibernateUtilities.getSessionFactory().openSession();
 		
 		session.beginTransaction();
-		
 		User user = new User();
 		user.setName("Joe");
 		user.setGoal(250);
 		session.save(user);
-		
 		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		User loadedUser = (User) session.get(User.class, 1);  //or session.load(User.class, 1), here 1 is id
+		System.out.println("loaded user:" + loadedUser.getName());
+		loadedUser.setTotal(loadedUser.getTotal() + 50);
+		session.getTransaction().commit();
+		
 		session.close();
 		
 		HibernateUtilities.getSessionFactory().close();
