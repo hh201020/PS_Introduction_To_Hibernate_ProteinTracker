adfedd47fbcd1055e48567bd144536e171fabd16
Jerry Hu <hh201020@gmail.com>
2016-10-03 11:46:57 -0400

    First Commit

---
+++ src/com/simpleprogramer/HibernateUtilities.java
@@ -0,0 +1,30 @@
+package com.simpleprogramer;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtilities {
+	
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+	
+	static
+	{
+		try{
+			Configuration configuration = new Configuration().configure();
+			
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		catch(HibernateException hibernateException){
+			System.out.println("Problem creating session factory: " + hibernateException.getMessage());
+		}
+	}
+	
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+}
